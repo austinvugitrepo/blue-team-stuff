448b28db1d3fc8c9856da085ce941df92e5a84b1
Austin Vu <freeluck.v.i.p999@gmail.com>
2026-04-02 13:06:34 -0500

    updated systemd script so its loopable

--- forsystemd.sh
+++ forsystemd.sh
@@ -1,18 +1,27 @@
 #!/bin/sh
-echo 'How do you want to list all services?:'
-echo 'A. active services'
-echo 'B. inactive services'
-read choice
 
-if [ -z "$choice" ]; then
-	echo 'Bye!'
-fi
+choice="idk" #choice filled with random string
 
-if [ "$choice" = "a" ] || [ "$choice" = "A" ]; then
-	systemctl list-units --all --type=service | grep -w 'active' #grep -w means search exactly for active and not include in(active) too
+while [ "$choice" != "q" ]
+ do
+	echo 'How do you want to list all services?:'
+	echo 'A. active services'
+	echo 'B. inactive services'
+        echo 'Your choice:';read choice
+	if [ "$choice" = "q" ]; then   # press q to quit
+                echo 'Bye!'
+                exit 0
+        fi
+	if [ -z "$choice" ]; then  # if user presses nothing
+		echo 'No response. Try again.'
+	fi
 
-elif [ "$choice" = "b" ] || [ "$choice" = "B" ]; then
-	systemctl list-units --all --type=service | grep -w 'inactive'
-else
-	echo 'Try again.'
-fi
+	if [ "$choice" = "a" ] || [ "$choice" = "A" ]; then
+		systemctl list-units --all --type=service | grep -w 'active' #grep -w means search exactly for active and not include in(active) too
+
+	elif [ "$choice" = "b" ] || [ "$choice" = "B" ]; then
+		systemctl list-units --all --type=service | grep -w 'inactive'
+	else
+		echo 'Wrong selection. Try again.'
+	fi
+ done
