e3104b8ae22d48ce0b18067e8e88566a330a7d3d
Austin Vu <freeluck.v.i.p999@gmail.com>
2026-04-02 14:33:29 -0500

    the qol update that adds header to list of services and adds clear screen option and shows q to quit

--- forsystemd.sh
+++ forsystemd.sh
@@ -8,23 +8,29 @@ while [ "$choice" != "q" ]
 	echo 'A. active services'
 	echo 'B. inactive services'
 	echo 'S. search service'
+	echo 'C. clear terminal'
+	echo 'q. press q to quit'
         echo 'Your choice:';read choice
 	if [ "$choice" = "q" ]; then   # press q to quit
                 echo 'Bye!'
                 exit 0
         fi
-
-	if [ "$choice" = "s" ] || [ "$choice" = "S" ]; then
+        
+	if [ "$choice" = "c" ] || [ "$choice" = "C" ]; then
+		clear
+	elif [ "$choice" = "s" ] || [ "$choice" = "S" ]; then
 		echo 'Enter search keyword:'
 		read searchkeyword
-		systemctl list-units --all --type=service | grep $searchkeyword
+		systemctl list-units --all --type=service | grep $searchkeyword | awk 'BEGIN{print "| SERVICE | LOAD | ACTIVE | STATE | DESC |"}{print $0}' #put value here
 		
 	elif [ "$choice" = "a" ] || [ "$choice" = "A" ]; then
-		systemctl list-units --all --type=service | grep -w 'active' #grep -w means search exactly for active and not include in(active) too
-
+		systemctl list-units --all --type=service | grep -w 'active' | awk 'BEGIN{print "| SERVICE | LOAD | ACTIVE | STATE | DESC |"}{print $0}' #grep -w means search exactly for active and not include in(active) too
+                
 	elif [ "$choice" = "b" ] || [ "$choice" = "B" ]; then
-		systemctl list-units --all --type=service | grep -w 'inactive'
+		systemctl list-units --all --type=service | grep -w 'inactive' | awk 'BEGIN{print "| SERVICE | LOAD | ACTIVE | STATE | DESC |"}{print $0}'
+		
 	else
 		echo 'Wrong selection. Try again.'
+		
 	fi
  done
