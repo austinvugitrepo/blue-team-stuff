bf7617ed7ef96f43af07ef7243b61ff8ab4a0cdc
Austin Vu <freeluck.v.i.p999@gmail.com>
2026-04-16 14:31:07 -0500

    updated my packages list and updated errparser to read more

--- errparser.sh
+++ errparser.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-find /var/log -type f | sed -e '/.journal/d' | xargs grep --color -e "fail" -e "error" -e "login"
+find /var/log -type f | sed -e '/.journal/d' | xargs grep --color -e "password" -e "error" -e "login" -e "user" -e "failed" -e "invalid"
